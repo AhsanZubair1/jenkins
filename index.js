5c04f468ebac118d82c1ec45df21542bec9cbfb0
AhsanZubair1 <87973516+AhsanZubair1@users.noreply.github.com>
2024-11-18 21:29:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Hello PK</title>
-</head>
-<body>
-  <h1>Hello PK</h1>
-</body>
-</html>
+console.log("this is executed byb node")
